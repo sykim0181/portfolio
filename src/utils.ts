7033cab05cce8c4631ea43798c6f7014cc4f3f7d
sykim0181 <soyeon364@naver.com>
2025-04-15 23:24:06 +0900

    chore: tailwind 클래스 관련 라이브러리 설치 및 유틸함수 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import clsx from "clsx";
+import { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function convertToPeriodStr(start: string, end: string | null) {
   // YYYY-MM-DD 형식
   let period = `${start.slice(0, 4)}.${start.slice(5, 7)} -`;
@@ -21,4 +25,8 @@ export function checkIsMobile() {
   const isMobile = mobileRegex.some((regex) => agent.match(regex));
 
   return isMobile;
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
