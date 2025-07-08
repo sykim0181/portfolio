b2aea90d80377b697656044ff0491447064737c3
sykim0181 <soyeon364@naver.com>
2025-07-08 20:50:11 +0900

    fix: 프로젝트 이미지 가운데 정렬

--- src/components/project/ProjectImage.tsx
+++ src/components/project/ProjectImage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/utils/cn";
 import { useState } from "react";
 
 interface ProjectImageProps {
@@ -22,12 +23,13 @@ const ProjectImage = (props: ProjectImageProps) => {
       </div>
 
       <div
-        className={`w-dvw h-dvh fixed top-0 left-0 bg-[rgba(235,235,235,0.5)] cursor-pointer z-10 ${
-          showImage ? "flex" : "hidden"
-        }`}
+        className={cn([
+          "w-dvw h-dvh fixed top-0 left-0 bg-[rgba(235,235,235,0.5)] cursor-pointer z-10",
+          showImage ? "flex" : "hidden",
+        ])}
         onClick={() => setShowImage(false)}
       >
-        <img src={src} className="max-w-full object-contain" />
+        <img src={src} className="max-w-full max-h-full object-contain absolute top-1/2 left-1/2 -translate-1/2" />
       </div>
     </>
   );
