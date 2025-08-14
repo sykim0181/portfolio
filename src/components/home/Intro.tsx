1bf74550d65f13a38d93dba458d65a9ad1c0712f
sykim0181 <soyeon364@naver.com>
2025-08-14 16:33:01 +0900

    fix: intro 수정
    - 간단한 소개를 intro에 추가하도록 변경

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -1,23 +1,24 @@
-import { IoIosArrowRoundDown } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import Memoticon from "./Memoticon";
 
 const Intro = () => {
   return (
     <div id="first" className="sticky top-0 w-full h-dvh bg-white">
-      <div className="w-(--default-width) h-full max-w-(--max-width) my-0 mx-auto flex flex-col justify-center align-center">
-        <div className="flex-1 flex flex-col justify-center w-full relative">
-          <h1 className="text-start text-[15vw] md:text-[min(15vw,8rem)] font-[SBAggroB]">
-            Portfolio
-          </h1>
-          <p className="text-[0.8rem] xs:text-[1.2rem] md:text-[1.5rem]">
+      <div className="w-(--default-width) h-full max-w-(--max-width) my-0 mx-auto py-[2rem] flex flex-col">
+        <div className="flex-1 flex flex-col justify-center items-center gap-[1.5rem]">
+          <Memoticon className="w-[100px]" />
+          <p className="text-[1.2rem] font-bold">
             안녕하세요, 프론트엔드 개발자 김소연입니다 :)
           </p>
+          <p className="text-[3rem] font-bold">Frontend Developer</p>
+          <p className="text-center">
+            끈기와 성실함을 무기로, 주어진 문제를 집요하게 해결합니다.
+            <br />
+            사소한 아이디어라도 기술로 만들어내며, 매일 한 걸음씩 성장합니다.{" "}
+          </p>
         </div>
-        <div className="flex w-full align-end justify-between py-[2rem]">
-          <IoIosArrowRoundDown className="text-[3rem] xs:text-[3.5rem] md:text-[5rem]" />
-          <div className="flex flex-col xs:flex-row gap-0 xs:gap-[1rem] items-end xs:items-start text-[0.8rem] xs:text-[1rem] md:text-[1.2rem]">
-            <p>By</p>
-            <p className="italic">Soyeon Kim</p>
-          </div>
+        <div className="mx-auto w-fit rounded-full bg-black text-white p-[0.5rem] flex justify-center items-center">
+          <IoIosArrowDown className="w-[1.6rem] h-[1.6rem]" />
         </div>
       </div>
     </div>
