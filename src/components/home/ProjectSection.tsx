8775e36d70388f624855035353171e934bf049dd
sykim0181 <soyeon364@naver.com>
2025-08-14 17:10:21 +0900

    fix: 프로젝트 네비게이션(타입) 버튼 추가

--- src/components/home/ProjectSection.tsx
+++ src/components/home/ProjectSection.tsx
@@ -1,21 +1,50 @@
-import { projectData } from "@/data/project";
+import { PROJECT_DATA } from "@/data/project";
 import ProjectItem from "./ProjectItem";
+import { ComponentProps, useMemo, useState } from "react";
+import { cn } from "@/utils/cn";
 
 interface ProjectSectionProps {
   ref: React.RefObject<HTMLDivElement | null>;
 }
 
+const projectTypes = ["All", "Personal", "Team", "Work"] as const;
+type ProjectType = (typeof projectTypes)[number];
+
 const ProjectSection = ({ ref }: ProjectSectionProps) => {
+  const [selectedType, setSelectedType] = useState<ProjectType>("All");
+
+  const projectData = useMemo(() => {
+    if (selectedType === "All") {
+      return PROJECT_DATA;
+    }
+    return PROJECT_DATA.filter(
+      (project) => project.type === selectedType.toUpperCase()
+    );
+  }, [selectedType]);
+
+  const onClickNav = (type: ProjectType) => setSelectedType(type);
+
   return (
-    <section 
-      id="projects" 
+    <section
+      id="projects"
       ref={ref}
       className="relative w-full box-border py-[3rem] z-1 bg-(--bg-color)"
     >
       <h2 className="text-white font-[PartialSansKR-Regular] text-[min(15vw,6rem)] font-normal">
         Project
       </h2>
-      <div className="w-(--default-width) max-w-(--max-width) flex flex-col items-center mx-auto mt-[2rem]">
+      <div className="w-(--default-width) max-w-(--max-width) flex flex-col items-center gap-[1.5rem] mx-auto mt-[2rem]">
+        <div className="flex gap-[1rem] w-full">
+          {projectTypes.map((type) => (
+            <ProjectNav
+              isSelected={type === selectedType}
+              onClick={() => onClickNav(type)}
+            >
+              {type}
+            </ProjectNav>
+          ))}
+        </div>
+
         <div className="grid gap-[min(10vw,3rem)] w-full grid-cols-2 md:grid-cols-3">
           {projectData.map((project) => (
             <ProjectItem
@@ -29,6 +58,25 @@ const ProjectSection = ({ ref }: ProjectSectionProps) => {
       </div>
     </section>
   );
+};
+
+interface ProjectNavProps extends ComponentProps<"div"> {
+  isSelected?: boolean;
 }
+
+const ProjectNav = (props: ProjectNavProps) => {
+  const { isSelected, children, ...otherProps } = props;
+  return (
+    <div
+      className={cn(
+        "border-1 rounded-[10px] px-[0.8rem] py-[0.3rem] cursor-pointer",
+        isSelected && "bg-black text-white"
+      )}
+      {...otherProps}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default ProjectSection;
--- src/data/project.ts
+++ src/data/project.ts
@@ -1,51 +1,63 @@
-export const projectData: {
+type ProjectType = "PERSONAL" | "TEAM" | "WORK";
+
+export const PROJECT_DATA: {
   id: number;
   name: string;
   imgSrc: string;
+  type: ProjectType;
 }[] = [
   {
     id: 9,
     name: "BIZKET",
-    imgSrc: "/projects/bizket.png"
+    imgSrc: "/projects/bizket.png",
+    type: "TEAM",
   },
   {
     id: 8,
     name: "Retro-Board",
     imgSrc: "/projects/retro-board.png",
+    type: "PERSONAL",
   },
   {
     id: 7,
     name: "포트폴리오",
     imgSrc: "/projects/my-portfolio.png",
+    type: "PERSONAL",
   },
   {
     id: 5,
     name: "GAIA Office 선 도형 기능 고도화",
     imgSrc: "/projects/advancing-connector.gif",
+    type: "WORK",
   },
   {
     id: 6,
     name: "GAIA Office 기능 유지 보수",
     imgSrc: "/projects/gaia-office.png",
+    type: "WORK",
   },
   {
     id: 1,
     name: "music-archiving",
     imgSrc: "/projects/music-archiving.gif",
+    type: "PERSONAL",
   },
   {
     id: 2,
     name: "이커머스 플랫폼",
     imgSrc: "/projects/xso.webp",
+    type: "PERSONAL",
   },
   {
     id: 3,
     name: "Aiku",
     imgSrc: "/projects/aiku.webp",
+    type: "TEAM",
   },
   {
     id: 4,
     name: "SyncSchedule",
     imgSrc: "/projects/sync-schedule.webp",
+    type: "TEAM",
   },
 ];
