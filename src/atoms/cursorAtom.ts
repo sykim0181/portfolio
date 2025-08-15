7db84456dd1bf18521e2fc16caa991010a72478a
sykim0181 <soyeon364@naver.com>
2025-08-15 16:17:09 +0900

    chore: 사용하지 않는 cursorPositionAtom 변수 제거

--- src/atoms/cursorAtom.ts
+++ src/atoms/cursorAtom.ts
@@ -1,10 +1,6 @@
 import { atom } from "jotai";
-import { Position } from "@/types/common";
 
 export type CursorType = "default" | "project" | "none";
 
-export const cursorPositionAtom = atom<Position | null>(null);
-
 export const cursorTypeAtom = atom<CursorType>("none");
-
 export const cursorTextAtom = atom<string>("");
