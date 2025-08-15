05710a22d5ddba46e1290e16292e47cdb326dd57
sykim0181 <soyeon364@naver.com>
2025-08-15 15:21:52 +0900

    feat: profile 데이터 분리 및 구분선 추가

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -1,52 +1,74 @@
 import { Montserrat } from "next/font/google";
 import { motion } from "motion/react";
 import { appearMotionProps } from "@/constants/motion";
-import { PROFILE_DATA } from "@/data/profile";
+import {
+  CAREER_DATA,
+  EDUCATION_DATA,
+  ETC_DATA,
+  TDataItem,
+} from "@/data/profile";
 
 const montserrat_italic = Montserrat({
   subsets: ["latin"],
   style: "italic",
 });
 
-const montserrat_bold = Montserrat({
-  subsets: ["latin"],
-  weight: "700",
-});
+// const montserrat_bold = Montserrat({
+//   subsets: ["latin"],
+//   weight: "700",
+// });
 
 const Profile = () => {
+  const Separator = () => <div className="w-full h-[.5px] bg-white" />;
+
   return (
     <motion.section {...appearMotionProps}>
-      <div className="w-(--default-width) max-w-(--max-width) mt-[3rem] mx-auto flex flex-col gap-[5rem]">
-        {PROFILE_DATA.map((sec) => (
-          <section key={`profile-${sec.category}`}>
-            <h4
-              className={`w-full text-[1.2rem] font-bold ${montserrat_italic.className}`}
-            >
-              {sec.category}
-            </h4>
-            <div className="mt-[1rem] flex flex-col gap-[1rem]">
-              {sec.items.map((item, idx) => (
-                <div key={`profile-data-${idx}`}>
-                  <div className="flex flex-col xs:grid xs:grid-cols-[1fr_2fr]">
-                    <p>{item.period}</p>
-                    <div>
-                      <p className="font-bold">{item.title}</p>
-                      {item.description.length > 0 && (
-                        <div className="text-gray-500">
-                          {item.description.map((des, idx) => (
-                            <p key={`description-${idx}`}>{des}</p>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </section>
-        ))}
+      <div className="w-(--default-width) max-w-(--max-width) mt-[3rem] mx-auto flex flex-col gap-[2.5rem]">
+        <ProfileSection sectionName="Career" items={CAREER_DATA} />
+        <Separator />
+        <ProfileSection sectionName="Education" items={EDUCATION_DATA} />
+        <Separator />
+        <ProfileSection sectionName="Etc." items={ETC_DATA} />
       </div>
     </motion.section>
+  );
+};
+
+interface ProfileSectionProps {
+  sectionName: string;
+  items: TDataItem[];
+}
+
+const ProfileSection = (props: ProfileSectionProps) => {
+  const { sectionName, items } = props;
+
+  return (
+    <section key={`profile-${sectionName}`}>
+      <h4
+        className={`w-full text-[1.2rem] font-bold ${montserrat_italic.className}`}
+      >
+        {sectionName}
+      </h4>
+      <div className="mt-[1rem] flex flex-col gap-[1rem]">
+        {items.map((item, idx) => (
+          <div key={`profile-data-${idx}`}>
+            <div className="flex flex-col xs:grid xs:grid-cols-[1fr_2fr]">
+              <p>{item.period}</p>
+              <div>
+                <p className="font-bold">{item.title}</p>
+                {item.description.length > 0 && (
+                  <div className="text-gray-500">
+                    {item.description.map((des, idx) => (
+                      <p key={`description-${idx}`}>{des}</p>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,51 +1,39 @@
-type TData = {
-  category: string;
-  items: {
-    period: string;
-    title: string;
-    description: string[];
-  }[];
+export type TDataItem = {
+  period: string;
+  title: string;
+  description: string[];
 };
 
-export const PROFILE_DATA: TData[] = [
+export const CAREER_DATA: TDataItem[] = [
   {
-    category: "Career",
-    items: [
-      {
-        period: "2024.04 - 2025.02",
-        title: "(주)티맥스가이아",
-        description: ["문서 협업 플랫폼 GAIA Docs의 오피스(문서 편집 앱) 개발"],
-      },
-    ],
+    period: "2024.04 - 2025.02",
+    title: "(주)티맥스가이아",
+    description: ["문서 협업 플랫폼 GAIA Docs의 오피스(문서 편집 앱) 개발"],
+  },
+];
+
+export const EDUCATION_DATA: TDataItem[] = [
+  {
+    period: "2019.03 - 2024.02",
+    title: "건국대학교 컴퓨터공학부",
+    description: [],
   },
   {
-    category: "Education",
-    items: [
-      {
-        period: "2019.03 - 2024.02",
-        title: "건국대학교 컴퓨터공학부",
-        description: [],
-      },
-      {
-        period: "2025.07 - 2025.08",
-        title: "부스트캠프 웹·모바일 10기 챌린지",
-        description: ["웹 풀스택(JavaScript)"],
-      },
-    ],
+    period: "2025.07 - 2025.08",
+    title: "부스트캠프 웹·모바일 10기 챌린지",
+    description: ["웹 풀스택(JavaScript)"],
   },
+];
+
+export const ETC_DATA: TDataItem[] = [
   {
-    category: "etc.",
-    items: [
-      {
-        period: "2022.09",
-        title: "정보처리기사",
-        description: [],
-      },
-      // {
-      //   period: "2024.01",
-      //   title: "영어 TOEIC",
-      //   description: ["975"],
-      // },
-    ],
+    period: "2022.09",
+    title: "정보처리기사",
+    description: [],
   },
+  // {
+  //   period: "2024.01",
+  //   title: "영어 TOEIC",
+  //   description: ["975"],
+  // },
 ];
