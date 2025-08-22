61f8bffdeee52f8c9622412cfc0baddabe162dd5
sykim0181 <soyeon364@naver.com>
2025-08-23 00:07:21 +0900

    feat: Contact 컴포넌트 스타일 수정

--- src/components/home/ContactSection.tsx
+++ src/components/home/ContactSection.tsx
@@ -34,36 +34,35 @@ const ContactSection = ({ ref }: ContactSectionProps) => {
     "relative after:block after:absolute after:h-[1px] after:w-[0] after:transition-[width] after:duration-500 hover:after:w-full hover:after:bg-black";
 
   return (
-    <section
-      id="contact"
-      ref={ref}
-      className="relative w-full h-dvh bg-(--bg-color) text-white z-1"
-    >
-      <div className="w-(--default-width) max-w-(--max-width) h-full grid grid-row-3 mx-auto">
-        <div className="row-start-2 row-end-3 flex justify-center items-center font-[PartialSansKR-Regular] text-[min(10vw,8rem)] text-center]">
-          {Message}
-        </div>
-        <div className="row-start-3 row-end-4 flex flex-col gap-[0.5rem] justify-start items-center">
-          <p>soyeon364@naver.com</p>
-          <div className="flex gap-[1rem]">
-            <a
-              href="https://github.com/sykim0181"
-              target="_blank"
-              className={socialLinkStyle}
-            >
-              Github
-            </a>
-            <a
-              href="https://sy-it.tistory.com/"
-              target="_blank"
-              className={socialLinkStyle}
-            >
-              Blog
-            </a>
+    <section ref={ref} className="relative w-full h-[200dvh] z-1">
+      <div id="contact" className="w-full h-dvh absolute bottom-0">
+        <div className="w-(--default-width) max-w-(--max-width) h-full grid grid-row-3 mx-auto">
+          <div className="row-start-2 row-end-3 flex justify-center items-center font-[PartialSansKR-Regular] text-[min(10vw,8rem)] text-center]">
+            {Message}
           </div>
-          <p className="text-[#aeaeae] text-[0.8rem]">©2025 KimSoyeon</p>
+          <div className="row-start-3 row-end-4 flex flex-col gap-[0.5rem] justify-start items-center">
+            <p>soyeon364@naver.com</p>
+            <div className="flex gap-[1rem]">
+              <a
+                href="https://github.com/sykim0181"
+                target="_blank"
+                className={socialLinkStyle}
+              >
+                Github
+              </a>
+              <a
+                href="https://sy-it.tistory.com/"
+                target="_blank"
+                className={socialLinkStyle}
+              >
+                Blog
+              </a>
+            </div>
+            <p className=" text-[0.8rem]">©2025 KimSoyeon</p>
+          </div>
         </div>
       </div>
+      <div className="w-full h-[200dvh] bg-linear-to-b from-white to-(--secondary-color) opacity-50"></div>
     </section>
   );
 };
