b4f7be117ffeee65e54ed20b9224430a0aa4b331
sykim0181 <soyeon364@naver.com>
2025-10-02 16:37:36 +0900

    chore: X-Robots-Tag 관련 nextconfig 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Robots-Tag", value: "noindex, nofollow, noarchive" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
